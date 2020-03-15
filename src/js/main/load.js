aef05e4331ca1b71cfd197ce36e64ad35a0ca74c
Arina Busygina <thr3@mail.ru>
2020-03-15 20:14:27 +0300

    Added styles

--- src/js/main/load.js
+++ src/js/main/load.js
@@ -1,9 +1,7 @@
 window.onload = onLoaded;
 
 const loader = document.querySelector('.loader');
-document.body.style.overflow = 'hidden';
 
 function onLoaded() {
-  document.body.style.overflow = '';
   loader.classList.add('loader--finished');
 }
